c1e581fa9b446faa52ecc759080cd705bb884a28
Shubham <shubhammishra41880@gmail.com>
2022-10-09 15:58:38 +0530

    find greater number

---
+++ print_the_gr8er_number.py
@@ -0,0 +1,7 @@
+num1 = int(input("please enter the first number :"))
+num2 = int(input("please enter the second number :"))
+
+if num1>num2:
+    print ("number1 is greater")
+else:
+    print ("number2 is greater")    
