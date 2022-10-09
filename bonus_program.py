54f698f45a600bdec585bac9ee13ee58c8cd7c58
Shubham <shubhammishra41880@gmail.com>
2022-10-09 13:28:05 +0530

    bonus calculation program

---
+++ bonus_program.py
@@ -0,0 +1,6 @@
+salary = int (input("enter your current salary :"))
+year = int (input("enter the no. of years you've been working in the company :"))
+if year>=5:
+    print ("your net salary is :",salary + 1000)
+else :
+    print ("your net salary is :",salary)    
