59f472e6cb91744f8985add881dbf8b804875045
Shubham <shubhammishra41880@gmail.com>
2022-10-19 22:57:29 +0530

    array practice

---
+++ arraypractice.py
@@ -0,0 +1,13 @@
+a = "the world shall know pain"
+
+print(a[3])#prints the letter with third index
+print(len(a))#prints the length of the string variable
+
+if "Shall" in a : #invert for this is 'not in'
+    print("yes its there")
+else:
+    print ("its not there")
+  
+  
+  
+#we prefer using list than array coz size manipulation during run time is possible
