eb77b936c5bcc82c868234d40a36d47024b5b0e8
Shubham <shubhammishra41880@gmail.com>
2022-11-12 21:18:41 +0530

    definig a multiplication function which returns value of product

---
+++ multiply_func_return.py
@@ -0,0 +1,10 @@
+def multiply(num1,num2):
+    product = num1*num2
+    return product
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+
+x = multiply(num1,num2)
+print(x)
