807ef7fdc7fd6358fe82f17c91b0abfdec20251e
Shubham <shubhammishra41880@gmail.com>
2022-11-20 11:00:28 +0530

    text type checker

---
+++ texttype_definer.py
@@ -0,0 +1,23 @@
+str1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+str2 = "1234567890"
+alph = 0
+num = 0
+def typetester():
+    x = str(input("please enter your text:"))
+for i in x:
+    if i in str1 and i not in str2:
+        alph+=1
+    elif i not in str1 and i  in str2:
+        num+=1
+if alph == 0 and num!=0:
+    print("the input text only consists of numerics")
+elif alph!=0 and num==0:
+    print("the input text only consists of alphabets")
+elif alph!=0 and num!=0 :
+    print("the input text consists of both alphabets and numerics so its a alpha numeric text")
+else : 
+    print("invalid data :(")
+print("number of alphabets:",alph)
+print("number of numerics:",num)
+typetester()
+
